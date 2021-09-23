d774e34032e6d513a71414abeedf5cc35206ee02
Sofiakb <contact.sofiakb@gmail.com>
2021-09-23 11:35:42 +0200

    Correctifs mineurs

--- src/Http/Controllers/ExampleController.php
+++ src/Http/Controllers/ExampleController.php
@@ -1,10 +1,8 @@
 <?php
 /**
  * This file contains ExampleController class.
- * Created by PhpStorm.
- * User: Sofiakb <contact.sofiak@gmail.com>
- * Date: HEADER_FILE_DATE
- * Time: HEADER_FILE_TIME
+ * @author Sofiakb <contact.sofiak@gmail.com>
+ * Created by PhpStorm on HEADER_FILE_DATE at HEADER_FILE_TIME
  */
 
 namespace EXAMPLE_NAMESPACE;
