981be1406df2dc8a18390cbedb8472c978d2a787
Sofiakb <contact.sofiakb@gmail.com>
2022-02-27 17:41:58 +0100

    Updated: minor

---
+++ src/Model.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * lumen-utils
+ * This file contains Model class.
+ *
+ * Created by PhpStorm on 27/02/2022 at 17:31
+ *
+ * @author Sofiakb <contact.sofiakb@gmail.com>
+ */
+
+namespace Sofiakb\Lumen\Utils;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Support\Carbon;
+
+/**
+ * Class Model
+ * @package Sofiakb\Lumen\Utils
+ * @author Sofiakb <contact.sofiakb@gmail.com>
+ *
+ * @method static Builder|static newModelQuery()
+ * @method static Builder|static newQuery()
+ * @method static Builder|static query()
+ * @mixin \Illuminate\Database\Query\Builder|Builder
+ 
+ * @property int $id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property Carbon|null $deleted_at
+ *
+ * @method static Builder|static where($param, $value = null)
+ * @method static Builder|static whereId($value)
+ * @method static Builder|static whereUpdatedAt($value)
+ * @method static Builder|static whereCreatedAt($value)
+ * @method static Builder|static whereDeletedAt($value)
+ * @method static Builder|static create(array $array)
+ * @method static Builder|static whereIn(string $column, mixed $values)
+ * @method static int count()
+ *
+ * @see Model
+ * @see Builder
+ */
+class Model extends BaseModel
+{
+
+}
